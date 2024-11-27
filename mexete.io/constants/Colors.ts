6a72c988da65700ab5ddbc5ef5cbc75818cdcb0c
mecls <carvalhal.busin@gmail.com>
2024-11-27 23:15:13 +0000

    Colors changed, accent color added, expo config done

--- mexete.io/constants/Colors.ts
+++ mexete.io/constants/Colors.ts
@@ -4,22 +4,22 @@
  */
 
 const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorDark = '#FFFFFF';
 
 export const Colors = {
   light: {
     text: '#11181C',
     background: '#fff',
     tint: tintColorLight,
-    icon: '#687076',
+    accent: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#FFFFFF',
+    background: '#131514',
     tint: tintColorDark,
-    icon: '#9BA1A6',
+    accent: '#FF914D',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
